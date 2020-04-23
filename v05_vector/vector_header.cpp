fd85b57f3d281f9e55b23147db48076dbe98d674
Aretas <57877668+aretka@users.noreply.github.com>
2020-04-23 15:19:28 +0300

    Add files via upload

---
+++ v05_vector/vector_header.cpp
@@ -0,0 +1,75 @@
+#include "vector_header.h"
+
+string IntToStr(int n)
+{
+    std::stringstream result;
+    result << n;
+    return result.str();
+}
+
+vector<Studentas> skirstykStudentusNetrinant(vector<Studentas>& studentai)
+{
+ vector<Studentas> kieti, minksti;
+ for (int i = 0; i != studentai.size(); i++)
+ {
+     if (studentai[i].getGalutinis() < 5)
+     minksti.push_back(studentai[i]);
+     else
+     kieti.push_back(studentai[i]);
+ }
+
+ studentai = kieti; // vektoriui 'studentai' priskiriame kietus
+ return minksti; // grąžina vektorių iš studentų gavusių skolą
+}
+
+vector<Studentas> skirstykStudentusTrinant(vector<Studentas>& studentai)
+{
+ vector<Studentas> minksti;
+ vector<Studentas>::size_type i = 0;
+ while(i!=studentai.size())
+ {
+     if (studentai[i].getGalutinis() < 5)
+	{
+ 	 minksti.push_back(studentai[i]);
+         i++;
+	}
+     else
+        break;
+ }
+ studentai.erase(studentai.begin(), studentai.begin() + i);
+ return minksti; // grąžina vektorių iš studentų gavusių skolą
+}
+
+/*void GalBalas(vector<Studentas>& studentai)
+{
+    int suma;
+    for(int i=0; i<studentai.size(); i++)
+    {
+        suma = 0;
+        for(int t=0; t<studentai[i].C.size(); t++)
+        {
+            suma+=studentai[i].C[t];
+        }
+        if(studentai[i].C.size() == 0)
+        {
+            studentai[i].setGalutinis(0.6*studentai[i].egzaminas);
+        }
+        else{
+            studentai[i].setGalutinis(0.4*suma/studentai[i].C.size())+(0.6*studentai[i].egzaminas);
+        }
+    }
+}*/
+
+bool comparePagalVarda( Studentas& x,  Studentas& y)
+{
+    return x.getVardas() < y.getVardas();
+}
+
+bool comparePagalPavarde( Studentas& x,  Studentas& y)
+{
+    return x.getPavarde() < y.getPavarde();
+}
+bool comparePagalGalutini( Studentas& x,  Studentas& y)
+{
+    return x.getGalutinis() < y.getGalutinis();
+}
---
+++ v05_vector/vector_header.h
@@ -0,0 +1,80 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+
+#include <bits/stdc++.h>
+#include <vector>
+#include <limits>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <string>
+#include <sys/time.h>
+
+
+using std::string;
+using std::vector;
+using std::deque;
+using std::list;
+using std::ofstream;
+using std::ifstream;
+using std::sort;
+using std::cout;
+using std::cin;
+using std::endl;
+
+class Studentas {
+// realizacija
+private:
+  string vardas;
+  string pavarde;
+  double egzaminas, galutinis;
+  vector<double> C;
+// interfeisas
+public:
+  void setVardas(string v)
+  {
+      vardas = v;
+  }
+  void setPavarde(string p)
+  {
+      pavarde = p;
+  }
+  void setGalutinis(double g)
+  {
+      galutinis = g;
+  }
+  void setEgzaminas(double e)
+  {
+      egzaminas = e;
+  }
+  string getVardas(){
+      return vardas;
+  }
+  string getPavarde(){
+      return pavarde;
+  }
+  double getGalutinis(){
+      return galutinis;
+  }
+  double getEgzaminas(){
+      return egzaminas;
+  }
+};
+
+/*struct Studentas{
+string vardas, pavarde;
+vector<int> C;
+int egzaminas;
+float galutinis;
+};*/
+
+string IntToStr(int );
+vector<Studentas> skirstykStudentusNetrinant(vector<Studentas>&);
+vector<Studentas> skirstykStudentusTrinant(vector<Studentas>&);
+//void GalBalas(deque<Studentas>& );
+bool comparePagalVarda( Studentas& ,  Studentas& );
+bool comparePagalPavarde( Studentas& ,  Studentas& );
+bool comparePagalGalutini( Studentas& ,  Studentas& );
+
+#endif
