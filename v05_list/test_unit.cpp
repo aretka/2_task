3757ce997bfeadb68f91de88a38457a9e7648455
Aretas <57877668+aretka@users.noreply.github.com>
2020-05-18 16:22:02 +0300

    Add files via upload

---
+++ v05_list/test_unit.cpp
@@ -0,0 +1,13 @@
+#include "list_header.h"
+#include "acutest.h"
+
+void test_add(void){
+    TEST_CHECK_(IntToStr(2) == "2", "IntToStr(%d)==(%s)", 2, "2");
+    TEST_CHECK_(IntToStr(20) == "20", "IntToStr(%d)==(%s)", 20, "20");
+    TEST_CHECK_(IntToStr(1000) == "1000", "IntToStr(%d)==(%s)", 1000, "1000");
+}
+
+TEST_LIST = {
+    {"string IntToStr(int);",test_add},
+    {0}
+};
